b7bad668f7c65e08197d5ed18c04e57f45a7c7dd
Jared Brown <jaobrown@iu.edu>
2018-09-16 22:37:46 -0400

    update
    updte

--- app/app.js
+++ app/app.js
@@ -1,46 +1,39 @@
 var allInfo = [];
 
+function modal() {
+    $('#addProduct').click(function (event) {
+        $('.modal').css({
+            'transform': 'translateX(0)'
+        });
+    });
+    $('.submit-btn').click(function (event) {
+        $('.modal').css({
+            'transform': 'translateX(100%)'
+        });
+    });
+}
+
 function renderProducts(product) {
     var newProductCard = "<div class=\"card\">";
     newProductCard += "<div class=\"card__img\">";
     newProductCard += "<svg class=\"card__img--icon\">";
-    newProductCard += "<use xlink:href=\"img/sprite.svg#icon-envelope\"></use>";
+    newProductCard += "<use xlink:href=\"img/sprite.svg#icon-" + product.image + "\"></use>";
     newProductCard += "</svg>";
     newProductCard += "</div>";
     newProductCard += "<div class=\"card__information\">";
     newProductCard += "<h1 class=\"card__title\">" + product.title + "</h1>";
-    newProductCard += "<p class=\"card__price\">" + product.price + "</p>";
+    newProductCard += "<p class=\"card__price\">$" + product.price + "</p>";
     newProductCard += "<p class=\"card__description\">" + product.description + "</p>";
-
-
-    // <div class="card__stars">
-    //     <svg class="card__stars--icon">
-    //     <use xlink:href="img/sprite.svg#icon-bolt"></use>
-    //     </svg>
-    //     <svg class="card__stars--icon">
-    //     <use xlink:href="img/sprite.svg#icon-bolt"></use>
-    //     </svg>
-    //     <svg class="card__stars--icon">
-    //     <use xlink:href="img/sprite.svg#icon-bolt"></use>
-    //     </svg>
-    //     <svg class="card__stars--icon">
-    //     <use xlink:href="img/sprite.svg#icon-bolt"></use>
-    //     </svg>
-    //     <svg class="card__stars--icon">
-    //     <use xlink:href="img/sprite.svg#icon-bolt"></use>
-    //     </svg>
-    //     </div>
-    //     </div>
-    //     </div>
-
-
-
-
-
-    newProductCard += "<p>" + product.title + ' ' + product.price + "</p>";
-    newProductCard += "<img src='" + product.image + "'/>";
-    newProductCard += "<p class='bio'>" + product.description + "</p>";
-    newProductCard += "</section>";
+    newProductCard += "<div class=\"card__stars\">";
+    // set # of stars
+    for (i = 0; i < product.stars; i++) {
+        newProductCard += "<svg class=\"card__stars--icon\">";
+        newProductCard += "<use xlink:href=\"img/sprite.svg#icon-bolt\"></use>";
+        newProductCard += "</svg>";
+    }
+    newProductCard += "</div>";
+    newProductCard += "</div>";
+    newProductCard += "</div>";
 
     $('.product-view').append(newProductCard);
 }
@@ -54,16 +47,17 @@ function init() {
     });
 
     initListeners();
+    alert("Click 'Add Product' to add a new item, Todd.");
 }
 
 function initListeners() {
     $('.submit-btn').click(function (e) {
         e.preventDefault();
-        var title = $('.product-form #title').val();
-        var price = $('.product-form #price').val();
-        var image = $('.product-form #image').val();
-        var description = $('.product-form #description').val();
-        var stars = $('.product-form #stars').val();
+        var title = $('.modal__form #title').val();
+        var price = $('.modal__form #price').val();
+        var image = $('.modal__form #image').val();
+        var description = $('.modal__form #description').val();
+        var stars = $('.modal__form #stars').val();
         var id = allInfo.length;
 
         var productObj = {
@@ -83,5 +77,5 @@ function initListeners() {
 $(document).ready(function () {
     console.log("init jquery");
     init();
-    console.log(allInfo);
+    modal();
 });
